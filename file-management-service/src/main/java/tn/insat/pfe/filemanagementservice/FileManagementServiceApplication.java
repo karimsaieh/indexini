7c7d228a13e39f8bc0121a4b6e85bfaeaad33037
Karim <ksayeh@ymail.com>
2019-02-14 17:19:53 +0100

    init file manager microservice

---
+++ file-management-service/src/main/java/tn/insat/pfe/filemanagementservice/FileManagementServiceApplication.java
@@ -0,0 +1,14 @@
+package tn.insat.pfe.filemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApplication.class, args);
+	}
+
+}
+
