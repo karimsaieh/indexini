b7906871f7eee074f3f6f2a4db52086c1ae89532
Karim <ksayeh@ymail.com>
2019-02-17 00:43:44 +0100

    init search service

---
+++ search-service/src/main/java/tn/insat/pfe/searchservice/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.insat.pfe.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
