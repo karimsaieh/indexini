40dd9d506b01866f0831a7d30ca22dfa3f9e8b8f
Karim <ksayeh@ymail.com>
2019-05-09 19:32:11 +0200

    777 waitforit

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose-staging.yml stop
-docker-compose -f docker-compose-staging.yml build
+docker-compose -f docker-compose-staging.yml pull
 docker-compose -f docker-compose-staging.yml up -d
