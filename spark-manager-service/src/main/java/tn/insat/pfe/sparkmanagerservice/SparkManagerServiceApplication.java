11ab4da52888cb45f4dda2601b157bc145dea9bf
Karim <ksayeh@ymail.com>
2019-05-25 01:07:51 +0200

    jaeger tracing in spring spark service

--- spark-manager-service/src/main/java/tn/insat/pfe/sparkmanagerservice/SparkManagerServiceApplication.java
+++ spark-manager-service/src/main/java/tn/insat/pfe/sparkmanagerservice/SparkManagerServiceApplication.java
@@ -1,12 +1,24 @@
 package tn.insat.pfe.sparkmanagerservice;
 
+import com.mongodb.MongoClient;
+import io.jaegertracing.Configuration;
+import io.opentracing.contrib.mongo.TracingMongoClient;
+import io.opentracing.contrib.mongo.common.TracingCommandListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 
 @EnableCircuitBreaker
 @SpringBootApplication
 public class SparkManagerServiceApplication {
+
+	@Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		io.opentracing.Tracer  tracer = Configuration.fromEnv().getTracer();
+
+		return tracer;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SparkManagerServiceApplication.class, args);
